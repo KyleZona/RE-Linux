339f910076d152dadc435603f1e1c59545633916
KyleZona <263722125+KyleZona@users.noreply.github.com>
2026-05-11 12:23:39 -0600

    Delete Easy directory
    rm, update 3.0 fixes bad_ending scripting dialogue, and only runs self-destruct-sequence.sh after player has cd ../ from Cultivation_Room

--- Easy/.Game_Files/Scenes/enter_bluedoor.sh
+++
@@ -1,7 +0,0 @@
-source "$GAME_DIR/.Game_Files/Design/typewriter_regular.sh"
-
-clear
-typewriter_regular ""
-typewriter_regular "        Barry: In that diary, they wrote about"
-typewriter_regular "          there being a way out through here..."
-typewriter_regular ""
--- Easy/.Game_Files/Scenes/enter_browndoor.sh
+++
@@ -1,8 +0,0 @@
-source "$GAME_DIR/.Game_Files/Design/typewriter_regular.sh"
-
-clear
-typewriter_regular ""
-typewriter_regular "			Barry: Jill, you really are the master of unlocking."
-typewriter_regular ""
-typewriter_regular "			Jill: Thanks, Barry."
-typewriter_regular ""
